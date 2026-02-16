d8788db3c9939416c1652671081b7ebd34efada0
Alejandro González <alejandrogq@mail.com>
2026-02-16 17:25:26 +0000

    Fix run_targeted_callbacks for Rails 5.2 callback API
    In Rails 5.2, CallbackChain#compile returns a CallbackSequence
    which does not respond to #call. Use invoke_before/invoke_after
    instead, matching the Rails 5.2 callbacks execution model.

--- lib/mongoid/interceptable.rb
+++ lib/mongoid/interceptable.rb
@@ -260,21 +260,17 @@ module Mongoid
           chain.append(callback) if callback.kind == place
         end
 
-        if Gem::Version.new("4.1.0") <= Gem::Version.new(ActiveSupport::VERSION::STRING)
-          self.class.send :define_method, name do
-            runner = ActiveSupport::Callbacks::Filters::Environment.new(self, false, nil)
-            chain.compile.call(runner).value
+        self.class.send :define_method, name do
+          env = ActiveSupport::Callbacks::Filters::Environment.new(self, false, nil)
+          sequence = chain.compile
+          if place == :before
+            sequence.invoke_before(env)
+          elsif place == :after
+            sequence.invoke_after(env)
           end
-          self.class.send :protected, name
-        else
-          class_eval <<-EOM
-            def #{name}()
-              #{chain.compile}
-            end
-            protected :#{name}
-          EOM
+          env.value
         end
-
+        self.class.send :protected, name
       end
       send(name)
     end
